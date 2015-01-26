ea36a295b3c2ebd9eb370611c9b1d292714f221e
Crispin Walker-Buckton <crispingiles@gmail.com>
2015-01-26 21:56:35 +0000

    Add more unit tests

--- SecretSanta.Core.Test/TestHasIdEqualityComparer.cs
+++ SecretSanta.Core.Test/TestHasIdEqualityComparer.cs
@@ -19,7 +19,27 @@ namespace CWB.SecretSanta.Core.Test
             var comparer = new HasIdEqualityComparer<Person>();
             var other = new Person {Id = 10};
             Assert.IsFalse(comparer.Equals(null, other), "One null and one non-null person should not compare as equal");
-            Assert.IsFalse(comparer.Equals(other, null), "One null and one non-null person should not compare as equal");
+            Assert.IsFalse(comparer.Equals(other, null), "One null and one non-null person should not compare as equal with operands reversed");
+        }
+
+        [Test]
+        public void TestDifferentIdsNotEquivalent()
+        {
+            var comparer = new HasIdEqualityComparer<Person>();
+            var one = new Person {Id = 10};
+            var two = new Person {Id = 20};
+            Assert.IsFalse(comparer.Equals(one, two), "Two person objects with different ids should not compare as equal");
+            Assert.IsFalse(comparer.Equals(two, one), "Two person objects with different ids should not compare as equal with operands reversed");
+        }
+
+        [Test]
+        public void TestEqualIdsEquivalent()
+        {
+            var comparer = new HasIdEqualityComparer<Person>();
+            var one = new Person {Id = 10};
+            var two = new Person {Id = 10};
+            Assert.IsTrue(comparer.Equals(one, two), "Two person objects with same ids should compare as equal");
+            Assert.IsTrue(comparer.Equals(two, one), "Two person objects with same ids should compare as equal with operands reversed");
         }
 
     }
